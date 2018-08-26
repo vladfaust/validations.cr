b9e5821d70237dbc9169a7c343f94c70df58a137
Vlad Faust <mail@vladfaust.com>
2018-08-26 14:17:31 +0300

    initial commit

---
+++ spec/validations_spec.cr
@@ -0,0 +1,135 @@
+require "./spec_helper"
+
+struct User
+  include Validations
+
+  enum Sex
+    Male
+    Female
+  end
+
+  property name : String
+  property email : String
+  @age : UInt8?
+  @sex : Sex
+  @inches : Float32? # Length of a user's... hair
+  setter age, sex, inches
+
+  def initialize(@name, @email, @sex : Sex, @age : UInt8? = nil, @inches : Float32? = nil)
+  end
+
+  validate name, size: (1..16)
+  validate email, size: (6..64), regex: /\w+@\w+\.\w{2,}/
+  validate @age, gte: 18
+  validate @sex, is: Sex::Male    # It's, um, boys' club
+  validate @inches, in: (6.5..20) # Only those guys with long hair are accepted
+
+  def validate
+    previous_def # Don't forget that
+
+    invalidate("name", "must not be equal to Vadim") if name == "Vadim" # Cheeki-Breeki
+  end
+end
+
+describe User do
+  ideal = User.new(name: "Vlad", email: "mail@vladfaust.com", age: 22, sex: :male)
+
+  context "when valid" do
+    it "is valid" do
+      ideal.valid?.should be_true
+    end
+
+    it "has empty invalid_attributes" do
+      ideal.invalid_attributes.empty?.should be_true
+    end
+  end
+
+  context "when with invalid name" do
+    user = ideal.dup
+    user.name = "Uvuvwevwevwe Onyetenyevwe Ugwemubwem Ossas"
+
+    it "is invalid" do
+      user.valid?.should be_false
+    end
+
+    it "raises" do
+      expect_raises Validations::Error do
+        user.valid!
+      end
+    end
+
+    it "has name in invalid_attributes" do
+      user.invalid_attributes.should eq ({"name" => ["must have size in (1..16)"]})
+    end
+  end
+
+  describe "custom validations" do
+    context "when with restricted name" do
+      user = ideal.dup
+      user.name = "Vadim"
+
+      it "is invalid" do
+        user.valid?.should be_false
+      end
+
+      it "has name in invalid_attributes" do
+        user.invalid_attributes.should eq ({"name" => ["must not be equal to Vadim"]})
+      end
+    end
+  end
+
+  context "when with invalid email" do
+    user = ideal.dup
+    user.email = "h@ck"
+
+    it "is invalid" do
+      user.valid?.should be_false
+    end
+
+    it "has email in invalid_attributes" do
+      user.invalid_attributes.should eq ({"email" => [
+        "must have size in (6..64)",
+        "must match /\\w+@\\w+\\.\\w{2,}/",
+      ]})
+    end
+  end
+
+  context "when with invalid age" do
+    user = ideal.dup
+    user.age = 17_u8
+
+    it "is invalid" do
+      user.valid?.should be_false
+    end
+
+    it "has age in invalid_attributes" do
+      user.invalid_attributes.should eq ({"@age" => ["must be greater than or equal to 18"]})
+    end
+  end
+
+  context "when with invalid sex" do
+    user = ideal.dup
+    user.sex = User::Sex::Female
+
+    it "is invalid" do
+      user.valid?.should be_false
+    end
+
+    it "has sex in invalid_attributes" do
+      user.invalid_attributes.should eq ({"@sex" => ["must be equal to Sex::Male"]})
+    end
+  end
+
+  context "when with too long hair" do
+    user = ideal.dup
+    user.inches = 29.5_f32
+
+    it "is invalid" do
+      user.valid?.should be_false
+    end
+
+    it "has inches in invalid_attributes" do
+      user.invalid_attributes.should eq ({"@inches" => ["must be in (6.5..20)"]})
+    end
+  end
+end
---
+++ src/validations.cr
@@ -0,0 +1,171 @@
+class Hash(K, V)
+  # Fetch *key* or set it to *value*.
+  #
+  # ```
+  # h = {"foo" => "bar"}
+  # h.fetch_or_set("foo", "baz"); pp h
+  # # {"foo" => "bar"}
+  # h.fetch_or_set("bar", "baz"); pp h
+  # # {"foo" => "bar", "bar" => "baz"}
+  # ```
+  def fetch_or_set(key, value)
+    if has_key?(key)
+      self.[key]
+    else
+      self.[key] = value
+    end
+  end
+end
+
+# Enables the including type to define attribute validations.
+#
+# ```
+# struct User
+#   include Validations
+#
+#   property name : String
+#   property email : String
+#   @age : UInt8?
+#   @nilable : String?
+#
+#   def initialize(@name, @email, @age : UInt8? = nil, @nilable : String? = nil)
+#   end
+#
+#   validate name, size: (1..16)
+#   validate email, size: (6..64), regex: /\w+@\w+\.\w{2,}/
+#   validate @age, gte: 18
+#
+#   # Will not be run if `@nilable.nil?`
+#   validate @nilable, size: (5..10)
+#
+#   # Custom validations are allowed
+#   def validate
+#     previous_def
+#     invalidate("name", "must not be equal to Vadim") if name == "Vadim"
+#   end
+# end
+#
+# user = User.new("Vadim", "e-mail", 17)
+# pp user.valid?
+# # false
+# pp user.invalid_attributes
+# # {
+# #   "name" => ["must have size in (1..16)", "must not be equal to Vadim"],
+# #   "email" => ["must have size in (6..64)", "must match /\\w+@\\w+\\.\\w{2,}/"],
+# #   "@age" => ["must be greater than or equal to 18"]
+# # }
+# ```
+module Validations
+  # Gently check if the including type is valid.
+  def valid?
+    validate
+    invalid_attributes.empty?
+  end
+
+  # Roughly check if the including type is valid, raising `Error` otherwise.
+  def valid!
+    valid? || raise Error.new(invalid_attributes)
+  end
+
+  # A hash of invalid attributes, if any.
+  #
+  # Example:
+  #
+  # ```
+  # pp user.invalid_attributes
+  # # {"name" => ["is too long"], ["is not Slav enough"]}
+  # ```
+  getter invalid_attributes = Hash(String, Array(String)).new
+
+  # Raised when the including type has validation errors after calling `valid!`.
+  class Error < Exception
+    # A hash of invalid attributes, similar to `Validations#invalid_attributes`.
+    getter invalid_attributes : Hash(String, Array(String))
+
+    def initialize(@invalid_attributes)
+    end
+  end
+
+  macro included
+    {% unless @type.has_method?("validate") %}
+      # Run validations, clearing `#invalid_attributes` before.
+      def validate
+        invalid_attributes.clear
+      end
+    {% end %}
+  end
+
+  # Mark *attribute* as invalid with *message*.
+  macro invalidate(attribute, message)
+    invalid_attributes.fetch_or_set({{attribute}}, Array(String).new).push({{message}})
+  end
+
+  # Validate *attribute* with inline *rules* unless it's `nil`.
+  #
+  # ```
+  # validate name, size: (1..16)
+  # validate email, regex: /\w+@\w+\.\w{2,}/
+  # validate age, gte: 18
+  # ```
+  #
+  # List of currently implemented inline rules:
+  #
+  # * `is: Object` - check if `attribute == object`
+  # * `gte: Comparable` - check if `attribute >= comparable`
+  # * `lte: Comparable` - check if `attribute <= comparable`
+  # * `gt: Comparable` - check if `attribute > comparable`
+  # * `lt: Comparable` - check if `attribute < comparable`
+  # * `in: Enumerable` - check if `enumerable.includes?(attribute)`
+  # * `size: Enumerable` - check if `enumerable.includes?(attribute.size)`
+  # * `regex: Regex` - check if `regex.match(attribute)`
+  #
+  # The `#validate` method can also be redefined to run custom validations:
+  #
+  # ```
+  # def validate
+  #   previous_def # Mandatory, otherwise previous validations won't run
+  #   invalidate("name", "some error") if name == "Foo"
+  # end
+  # ```
+  macro validate(attribute, **rules)
+    def validate
+      previous_def
+
+      unless {{attribute.id}}.nil?
+        value = {{attribute.id}}.not_nil!
+
+        {% if rules[:is] %}
+          invalidate({{attribute.stringify}}, "must be equal to {{rules[:is]}}") unless value == {{rules[:is]}}
+        {% end %}
+
+        {% if rules[:gte] %}
+          invalidate({{attribute.stringify}}, "must be greater than or equal to {{rules[:gte]}}") unless value >= {{rules[:gte]}}
+        {% end %}
+
+        {% if rules[:lte] %}
+          invalidate({{attribute.stringify}}, "must be less than or equal to {{rules[:lte]}}") unless value <= {{rules[:lte]}}
+        {% end %}
+
+        {% if rules[:gt] %}
+          invalidate({{attribute.stringify}}, "must be greater than {{rules[:gt]}}") unless value > {{rules[:gt]}}
+        {% end %}
+
+        {% if rules[:lt] %}
+          invalidate({{attribute.stringify}}, "must be less than {{rules[:lt]}}") unless value < {{rules[:lt]}}
+        {% end %}
+
+        {% if rules[:in] %}
+          invalidate({{attribute.stringify}}, "must be in {{rules[:in]}}") unless {{rules[:in]}}.includes?(value)
+        {% end %}
+
+        {% if rules[:size] %}
+          invalidate({{attribute.stringify}}, "must have size in {{rules[:size]}}") unless {{rules[:size]}}.includes?(value.size)
+        {% end %}
+
+        {% if rules[:regex] %}
+          invalidate({{attribute.stringify}}, "must match " + {{rules[:regex].stringify}}) unless ({{rules[:regex]}}).match(value)
+        {% end %}
+      end
+    end
+  end
+end
