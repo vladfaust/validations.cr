d53c8cd421c4e55947a32c3e7c89f95f4513e259
Vlad Faust <mail@vladfaust.com>
2019-03-30 16:55:22 +0300

    fixup: 2c46b49

--- src/validations/rules/scheme.cr
+++ src/validations/rules/scheme.cr
@@ -1,14 +1,16 @@
-require "URI"
+require "uri"
 
 module Validations
   protected def validate_scheme(attr, value, scheme : String | Array(String))
     return if value.nil?
+
     value_scheme = URI.parse(value).scheme
     return if value_scheme.nil?
+
     case scheme
     when String
       invalidate(attr, "must have scheme equal to #{scheme}") if value_scheme != scheme
-    when Array(String)
+    else
       invalidate(attr, "must have scheme in #{scheme}") unless scheme.includes?(value_scheme)
     end
   end
